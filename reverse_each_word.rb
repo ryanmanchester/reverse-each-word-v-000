d3b4391b387575170c97f80586aefea61517ebd6
Ryan Manchester <ryanm17@gmail.com>
2017-10-12 20:11:08 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -3,15 +3,11 @@ def reverse_each_word(phrase)
   arr = []
   phrase.split.each do |word|
     arr << "#{word.reverse}"
-
-  end
- arr.join(" ")
-
  end
-
- def reverse_collect(phrase)
-  phrase.split.collect do |word|
-    "#{word.reverse}"
-  end
+ arr.join(" ")
+ end
+ phrase.split.collect do |word|
+   "#{word.reverse}"
+ end
 
  end
